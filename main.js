b622d3b60616fc484c11ab14afc387d2bd7bf2df
Del-Valle <133610239+hielo5@users.noreply.github.com>
2024-06-28 22:53:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,138 @@
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Message In A Bottle",
+    artist: "The Police",
+    image: "https://fabulous-daffodil-8ff2bd.netlify.app/caratula/0001%20The%20Police%20-%20Message%20In%20A%20Bottle.jpg",
+    path: "https://fabulous-daffodil-8ff2bd.netlify.app/audio/0001%20The%20Police%20-%20Message%20In%20A%20Bottle.mp3"
+  },
+  {
+    name: "Música ligera",
+    artist: "Soda stereo",
+    image: "https://fabulous-daffodil-8ff2bd.netlify.app/caratula/0002%20soda%20stereo%20musica%20ligera.jpg",
+    path: "https://fabulous-daffodil-8ff2bd.netlify.app/audio/0002%20soda%20stereo%20musica%20ligera.mp3"
+  },
+  {
+    name: "Ayer Me Llamó Mi Ex",
+    artist: "KHEA ",
+    image: "https://fabulous-daffodil-8ff2bd.netlify.app/caratula/0003%20KHEA%20%20Ayer%20Me%20Llam%C3%B3%20Mi%20Ex.jpg",
+    path: "https://fabulous-daffodil-8ff2bd.netlify.app/audio/0003%20KHEA%20%20Ayer%20Me%20Llam%C3%B3%20Mi%20Ex.mp3",
+  },
+];
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  // Seleccionar el elemento <img> y actualizar su src
+  const track_art = document.getElementById('track_art');
+  track_art.src = track_list[track_index].image;
+  // Añadir clases para asegurar tamaño
+  track_art.className = "h-[200px] border";
+
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = (track_index + 1) + " / " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i>pausa</i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i>play</i>';
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
+
+
